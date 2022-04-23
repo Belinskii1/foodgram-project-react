a52d5ea8f2bee37e48c4739e2e7c8a309e52a829
Belinskii1 <belinskii1@yandex.ru>
2022-04-23 14:55:26 +0300

    user_model

---
+++ backend/users/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=25,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Псевдоним'
+    )
+    email = models.EmailField(
+        max_length=50,
+        unique=True,
+        verbose_name='Адрес почты'
+    )
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name='Имя пользователя'
+    )
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name='Фамилия'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
