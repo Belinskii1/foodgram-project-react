d3c2995cb8771224abdca1fb788cb0168a11c3b5
Belinskii1 <Belinskii1@yandex.ru>
2022-04-30 19:26:47 +0300

    recipe admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
-from .models import Tag, Ingredient
+from .models import (Tag, Ingredient, Recipe, 
+                     IngredientRecipe, TagRecipe)
+
+
+class TagRecipeInline(admin.TabularInline):
+    model = TagRecipe
+
+
+class IngredientRecipeInline(admin.TabularInline):
+    model = IngredientRecipe
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [
+        TagRecipeInline,
+        IngredientRecipeInline,
+    ]
+
 
 admin.site.register(Tag)
 admin.site.register(Ingredient)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(IngredientRecipe)
+admin.site.register(TagRecipe)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,4 +1,16 @@
+import os
+import base64
+from django.core import validators
 from django.db import models
+from django_base64field.fields import Base64Field
+
+def image_as_base64(image_file, format='png'):
+    if not os.path.isfile(image_file):
+        return None
+    encoded_string = ''
+    with open(image_file, 'rb') as img_f:
+        encoded_string = base64.b64encode(img_f.read())
+    return 'data:image/%s;base64,%s' % (format, encoded_string)
 
 
 class Tag(models.Model):
@@ -31,17 +43,17 @@ class Tag(models.Model):
         unique=True,
         blank=False,
         null=False,
-        verbose_name = 'Название Тега'
+        verbose_name = 'название Тега'
     )
     color = models.CharField(
         choices=COLOR_CHOICES,
         default=BLUE,
         max_length=10,
-        verbose_name='Цвет'
+        verbose_name='цвет'
     )
     slug = models.SlugField(
         unique=True,
-        verbose_name='Поле Slug'
+        verbose_name='поле Slug'
     )
     class Meta:
         verbose_name = 'Тег'
@@ -71,7 +83,7 @@ class Ingredient(models.Model):
         unique=True,
         blank=False,
         null=False,
-        verbose_name = 'Название Ингридиента'
+        verbose_name = 'название Ингридиента'
     )
     measurement_unit = models.CharField(
         choices=MEANSUREMENT_UNIT_CHOISES,
@@ -80,8 +92,73 @@ class Ingredient(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Ннгридиент'
+        verbose_name = 'Ингридиент'
         verbose_name_plural = 'Ингридиенты'
 
     def __str__(self):
         return self.name   
+
+
+class Recipe(models.Model):
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientRecipe',
+        verbose_name = 'ингридиенты'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        through='TagRecipe',
+        verbose_name = 'теги'
+    )
+    image = models.ImageField(
+        upload_to='images/',
+        verbose_name = 'картинка'
+        )
+    name = models.CharField(
+        max_length=70,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name = 'Название Рецепта'
+    )
+    text = models.TextField(
+        verbose_name = 'описание'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        validators=(
+            validators.MinValueValidator(
+                0, message='Укажите время приготовления блюда'),
+            ),
+        verbose_name='Время приготовления'
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name  
+
+
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Ингридиент в рецепте'
+        verbose_name_plural = 'Ингридиенты в рецептах'
+
+    def __str__(self):
+        return f'Рецепт {self.recipe.name} содержит ингридиент {self.ingredient.name}'
+
+
+class TagRecipe(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Тег у рецепта'
+        verbose_name_plural = 'Теги у рецептов'
+
+    def __str__(self):
+        return f'У рецепта {self.recipe.name} есть тег {self.tag.name}'
