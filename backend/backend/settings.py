5156a4c748deb1f852a098a2cc8ebc6b12789d7e
Belinskii1 <Belinskii1@yandex.ru>
2022-05-13 19:57:56 +0300

    fin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,23 +84,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
-        #'NAME': os.environ.get('DB_NAME'),
-        #'USER': os.environ.get('POSTGRES_USER'),
-        #'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        #'HOST': os.environ.get('DB_HOST'),
-        #'PORT': os.environ.get('DB_PORT'),
-    #}
-#}
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://*.127.0.0.1']
 
 
 # Password validation
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,6 +7,8 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+import reportlab
+from django.conf import settings
 
 from .filters import IngredientSearchFilter, RecipeFilter
 from .models import (Favorite, Ingredient, IngredientRecipe, Recipe,
@@ -90,34 +92,50 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'],
             permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        final_list = {}
         ingredients = IngredientRecipe.objects.filter(
-            recipe__shoppingcart__user=request.user).values_list(
-            'ingredient__name', 'ingredient__measurement_unit',
-            'amount')
-        for item in ingredients:
-            name = item[0]
-            if name not in final_list:
-                final_list[name] = {
-                    'measurement_unit': item[1],
-                    'amount': item[2]
-                }
-            else:
-                final_list[name]['amount'] += item[2]
-        pdfmetrics.registerFont(
-            TTFont('Tahoma', 'Tahoma.ttf', 'UTF-8'))
-        response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = ('attachment; '
-                                           'filename="shopping_list.pdf"')
-        page = canvas.Canvas(response)
-        page.setFont('Tahoma', size=24)
-        page.drawString(200, 800, 'Список ингредиентов')
-        page.setFont('Tahoma', size=16)
-        height = 750
-        for i, (name, data) in enumerate(final_list.items(), 1):
-            page.drawString(75, height, (f'{i}. {name} - {data["amount"]}, '
-                                         f'{data["measurement_unit"]}'))
-            height -= 25
-        page.showPage()
-        page.save()
+            recipe__shoppingcart__user=request.user).values(
+            'ingredient__name', 'ingredient__measurement_unit', 'amount'
+        )
+        shopping_cart = '\n'.join([
+            f'{ingredient["ingredient__name"]} - {ingredient["amount"]} '
+            f'{ingredient["ingredient__measurement_unit"]}'
+            for ingredient in ingredients
+        ])
+        filename = 'shopping_cart.txt'
+        response = HttpResponse(shopping_cart, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename={filename}'
         return response
+
+    # def download_shopping_cart(self, request):
+    #     final_list = {}
+    #     ingredients = IngredientRecipe.objects.filter(
+    #         recipe__shoppingcart__user=request.user).values_list(
+    #         'ingredient__name', 'ingredient__measurement_unit',
+    #         'amount')
+    #     for item in ingredients:
+    #         name = item[0]
+    #         if name not in final_list:
+    #             final_list[name] = {
+    #                 'measurement_unit': item[1],
+    #                 'amount': item[2]
+    #             }
+    #         else:
+    #             final_list[name]['amount'] += item[2]
+    #     reportlab.rl_config.TTFSearchPath.append(str(settings.BASE_DIR) + '/backend/libes/fonts')
+    #     pdfmetrics.registerFont(
+    #         TTFont('tahoma', 'tahoma.ttf', 'UTF-8'))
+    #     response = HttpResponse(content_type='application/pdf')
+    #     response['Content-Disposition'] = ('attachment; '
+    #                                        'filename="shopping_list.pdf"')
+    #     page = canvas.Canvas(response)
+    #     page.setFont('tahoma', size=24)
+    #     page.drawString(200, 800, 'Список ингредиентов')
+    #     page.setFont('tahoma', size=16)
+    #     height = 750
+    #     for i, (name, data) in enumerate(final_list.items(), 1):
+    #         page.drawString(75, height, (f'{i}. {name} - {data["amount"]}, '
+    #                                      f'{data["measurement_unit"]}'))
+    #         height -= 25
+    #     page.showPage()
+    #     page.save()
+    #     return response
